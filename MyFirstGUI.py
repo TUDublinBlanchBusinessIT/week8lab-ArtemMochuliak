2bee0655e1d13f14b09dab101f300979b75b13e9
B00165618 Artem Mochuliak <B00165618@mytudublin.ie>
2025-03-24 16:40:26 +0000

    Added GUI fields amd layout complete

--- MyFirstGUI.py
+++ MyFirstGUI.py
@@ -16,14 +16,30 @@ class MyFirstGUI:
         self.entry2 = Entry()
         self.entry2.pack()
         
-        self.helloButton = Button(master, text="Hello", command=self.sayhello)
-        self.helloButton.pack()
+        self.label3 = Label(master, text="Enter your Date of Birth")
+        self.label3.pack()
+        self.entry3 = Entry()
+        self.entry3.pack()
+
+        self.label4 = Label(master, text="Enter your Member Type")
+        self.label4.pack()
+        self.entry4 = Entry()
+        self.entry4.pack()
+        
+        self.insertButton = Button(master, text="Insert Into DB", command=self.insert_into_db)
+        self.insertButton.pack()
+
+        self.printButton = Button(master, text="Print All Members", command=self.print_all)
+        self.printButton.pack()
 
         self.closeButton = Button(master, text="Close", command=self.close)
         self.closeButton.pack()
-
-    def sayhello(self):    
-        print("Hello " + self.entry1.get())
+        
+    def insert_into_db(self):    
+       
+        
+    def print_all(self):    
+       
 
     def close(self):
         root.destroy()
